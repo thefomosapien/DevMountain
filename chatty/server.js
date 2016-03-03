3c49b1394e2ecae42fbe5907ee9743f9553ce17f
Corey Robison <coreybrobison@gmail.com>
2016-03-02 18:47:08 -0700

    my first app with node

--- chatty/server.js
+++ chatty/server.js
@@ -1,1 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(bodyParser.json());
+
+var messages = ['la la la'];
+
+var port = 8989;
+
+app.listen(port, function () {
+    console.log("Listening on", port)
+})
+
+app.get('/', function (req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+app.post('/', function (req, res) {
+
+    messages.push({
+        message: req.body.message,
+        time: new Date()
+    });
+
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+
+});
+
+app.options('/', function (req, res) {
+    res.status(200).set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }).send(JSON.stringify(messages));
+})
