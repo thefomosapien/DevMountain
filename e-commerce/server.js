e041ca5b3a238e5d9b9ffa56258d514bf72e7b2f
Corey Robison <coreybrobison@gmail.com>
2016-03-07 16:43:18 -0700

    Working with MongoDB

---
+++ e-commerce/server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var mongojs = require("mongojs");
+
+var app = express();
+var port = 3000;
+var db = mongojs("eCommerce");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, function() {
+    console.log('Now listening on port: ' + port);
+});
+
+app.post('api/products', function(req, res) {
+    res.send('');
+});
+
+app.get('/products', function(req, res) {
+    res.send('');
+});
+
+app.put('/products/:id', function(req, res) {
+    
+});
+
+app.delete('/products/:id', function(req, res) {
+    
+});
+
--- mini-birds/server.js
+++ mini-birds/server.js
@@ -1,24 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongojs = require('mongojs');
-var cors = require('cors');
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var mongojs = require("mongojs");
 
 var app = express();
 app.use(bodyParser.json());
 
-var nodepPort = 3000;
-
-var db = mongojs('bird-sightings');
-
-var collection = db.collection('sightings');
+var nodePort = 3000;
 
 
+var db = mongojs("bird-sightings");
+var collection = db.collection("sightings");
 
-app.post('api/sighting', function (req, res) {
+app.post('/api/sighting', function (req, res) {
     console.log(req.body);
     collection.insert(req.body, function (err, r) {
         console.log(r);
-        return res.status(200).send('POST');
+        return res.status(200).send(r);
     })
 
 });
@@ -40,5 +38,6 @@ app.delete('/api/sighting?id=09evasd09jhahs9d8h9vh', function (req, res) {
 });
 
 
-console.log('listening port 3000')
-app.listen(nodepPort);
+app.listen(nodePort, function () {
+    console.log('listening on port ' + nodePort);
+});
