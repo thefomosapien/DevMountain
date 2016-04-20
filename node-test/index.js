cac12b19f8326ebbd9f1b6477c8b24944a4e7e10
Corey Robison <coreybrobison@gmail.com>
2016-04-20 11:13:58 -0600

    nodemon

---
+++ node-test/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log('listening on port 3000');
+})
