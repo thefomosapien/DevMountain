a0636d67a9379e69b021b423f58fb76dd7453208
Corey Robison <coreybrobison@gmail.com>
2016-04-20 11:41:57 -0600

    get and post

--- node-test/index.js
+++ node-test/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var users = [];
+
 var app = express();
 
+app.use(bodyParser.json());
 
+app.get('/', function (req, res, next) {
+    res.status(200).json({message: 'hello'});
+});
 
+app.post('/users', function(req, res, next) {
+    users.push(req.body);
+    res.status(200).json(users)
+});
 
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('listening on port 3000');
-})
+});
